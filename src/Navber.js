7006c3229150a7d5e3b92c1a91eb670bddf39a45
syedfawadhashmi5 <59658569+syedfawadhashmi5@users.noreply.github.com>
2020-07-22 07:46:50 -0700

    Update Navber.js

--- src/Navber.js
+++ src/Navber.js
@@ -19,19 +19,19 @@ const Navber = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                   <ul className="navbar-nav ml-auto">
                     <li className="nav-item">
-                      <Link ClassName="Menu_active" className="nav-link active" to="/">Home <span className="sr-only">(current)</span></Link>
+                      <Link ClassName="Menu_active" className="nav-link active" to="/">Home <span className="sr-only"></span></Link>
                     </li>
                     <li className="nav-item">
-                      <Link className="Menu_active" className="nav-link active" to="/About">About <span className="sr-only">(current)</span></Link>
+                      <Link className="Menu_active" className="nav-link active" to="/About">About <span className="sr-only"></span></Link>
                     </li>
                     <li className="nav-item">
-                      <Link className="Menu_active" className="nav-link active" to="/Career">Career <span className="sr-only">(current)</span></Link>
+                      <Link className="Menu_active" className="nav-link active" to="/Career">Career <span className="sr-only"></span></Link>
                     </li>
                     <li className="nav-item">
-                      <Link className="Menu_active" className="nav-link active" to="/Contect">Contect <span className="sr-only">(current)</span></Link>
+                      <Link className="Menu_active" className="nav-link active" to="/Contect">Contect <span className="sr-only"></span></Link>
                     </li>
                     <li className="nav-item">
-                      <Link className='Menu_active' className="nav-link active" to="/Services">Services <span className="sr-only">(current)</span></Link>
+                      <Link className='Menu_active' className="nav-link active" to="/Services">Services <span className="sr-only"></span></Link>
                     </li>
                   </ul>
                 </div>
