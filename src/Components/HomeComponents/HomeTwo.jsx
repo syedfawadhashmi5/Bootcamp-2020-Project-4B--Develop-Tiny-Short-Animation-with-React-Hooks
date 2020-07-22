32b8a3a06f60ccbe39c406cf56a1009f3b23d217
syedfawadhashmi5 <59658569+syedfawadhashmi5@users.noreply.github.com>
2020-07-22 07:32:17 -0700

    Update HomeTwo.jsx

--- src/Components/HomeComponents/HomeTwo.jsx
+++ src/Components/HomeComponents/HomeTwo.jsx
@@ -15,7 +15,7 @@ const HomeTwo = () => {
                                 <div className="col-md-6 pt-5 pt-lg-0 order-1 order-lg-1 d-flex ">
                                 </div>
                                 <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-2 d-flex justify-content-center flex-colum">
-                                    <span className="abtimg"><Fade bottom><img className="img-fluid"   src={img2} alt="about image" /></Fade></span>
+                                    <span className="abtimg"><Fade bottom><img className="img-fluid"   src={img2} alt="about" /></Fade></span>
                                 </div>
                                 <div class="col-md-6 hdAndText"><Fade bottom><h1 class="h1text">We Are</h1></Fade>
                                 <Fade bottom><p>A highly specialized Management team with over 20 combined years of experience in the Healthcare industry with proven ability to optimally combine digital technology, skilled resources and efficient business processes to implement creative as well as proven market solutions to suit an ever-evolving market. Our forte has been to successfully tailor our solutions to support your needs whether it is in the space of Consulting, Product Management or Service Delivery</p></Fade>
